9612f3b3ad06db7fd68618d29fd0f0dcec6692b3
Nha San <sotpanhnha@gmail.com>
2023-11-03 19:45:47 +0700

    add activitys

---
+++ app/src/main/java/com/admin/admineventmanagement/ui/activitys/ScanQRActivity.kt
@@ -0,0 +1,12 @@
+package com.admin.admineventmanagement.ui.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.admin.admineventmanagement.R
+
+class ScanQRActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_qractivity)
+    }
+}
